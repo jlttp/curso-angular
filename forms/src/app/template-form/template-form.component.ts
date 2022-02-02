7b9c5ca67e2fa93ff2f3d3930bf0f0fd5fd6379c
jlttp <juuliette@gmail.com>
2022-02-02 13:43:32 -0300

    Aula #78 - Forms (template driven) Aplicando CSS na validação dos campos

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -9,8 +9,8 @@ import { NgForm } from '@angular/forms';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'Juliette',
-    email: 'juliette@email.com'
+    nome: null,
+    email: null
   }
 
   constructor() { }
