29468ddfb207962ff420405aef505d93fe400d66
jlttp <juuliette@gmail.com>
2022-02-04 12:16:57 -0300

    Aula #81 - Forms (Dica): Verificando dados do Form no template com JSON

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -4,18 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     DataFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    TemplateFormModule,
     AppRoutingModule
   ],
   providers: [],
