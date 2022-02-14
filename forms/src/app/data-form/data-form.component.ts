d2903bb70e0300df3def8ecf0a90687d7dbef0f0
jlttp <juuliette@gmail.com>
2022-02-14 09:18:21 -0300

    Aula #110 - Formulários reativos: Validação entre dois campos (confirmar email)

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -55,6 +55,7 @@ export class DataFormComponent implements OnInit {
     this.formulario = this.formBuilder.group({
       nome: [null, [Validators.required]],
       email: [null, [Validators.required, Validators.email]],
+      confirmarEmail: [null, [Validators.required, Validators.email, FormValidations.equalsTo('email')]],
       endereco: this.formBuilder.group({
       cep: [null, [Validators.required, FormValidations.cepValidator]],
       numero: [null, [Validators.required]],
