b50b5f176384f487855bfebf20b2f6fc89f95722
jlttp <juuliette@gmail.com>
2022-02-24 09:57:26 -0300

    Aula #139 - Upload Arquivo: Barra de Progresso + Observando Eventos Http

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UploadFileService } from '../upload-file.service';
 import { environment } from '../../../environments/environment';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { AlertModalService } from '../../shared/alert-modal.service';
 
 @Component({
   selector: 'app-upload-file',
@@ -10,8 +12,12 @@ import { environment } from '../../../environments/environment';
 export class UploadFileComponent implements OnInit {
 
   files!: Set<File>;
+  progress = 0;
 
-  constructor(private service: UploadFileService) { }
+  constructor(
+    private service: UploadFileService,
+    private alertService: AlertModalService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -29,12 +35,25 @@ export class UploadFileComponent implements OnInit {
       this.files.add(selectedFiles[i]);
     }
 
+    this.progress = 0;
+
   }
 
   onUpload(){
     if(this.files && this.files.size > 0){
       this.service.upload(this.files, `${environment.BASE_URL}/upload`) //'http://localhost:8000/upload')
-        .subscribe(response => console.log('Upload concluído.'));
+        .subscribe((event: HttpEvent<Object>) => {
+          //HttpEventType.UploadProgress
+          console.log(event);
+          if(event.type === HttpEventType.Response){
+            console.log('Upload concluído.');
+            this.alertService.showAlertSuccess('Upload concuído com sucesso!');
+          } else if(event.type === HttpEventType.UploadProgress){
+            const percentDone = Math.round((event.loaded * 100) / event.total!);
+            console.log(`Progresso: ${percentDone}`);
+            this.progress = percentDone;
+          }
+        });
     }
   }
 
